acaf7f31075cfde3013b6a034d41ad01332e11b6
Paul <paulglujan@gmail.com>
2022-12-07 16:18:05 -0800

    parseCSV requirements

---
+++ utilities/parseCsv.js
@@ -0,0 +1,14 @@
+// Given a CSV file with a row of dates, column of times, and checkboxes at
+// intersections, print out an array of timestamps for each cell.
+
+// Example Input
+/************************************************** 
+* ,12/7/22,12/8/22
+* 7:00,TRUE,FALSE
+* 7:15,FALSE,TRUE
+**************************************************/
+
+// Example Output
+/************************************************** 
+* [12/7/22 7:00, 12/8/22 7:15]
+**************************************************/
